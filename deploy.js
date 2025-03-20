c1a47e162765b825534ccbb3d388d0f20b36394c
Egor Solonin <e.solonin@onpoint.ru>
2025-03-20 16:49:29 +0300

    add deploy scripts

---
+++ deploy.js
@@ -0,0 +1,15 @@
+const hre = require('hardhat')
+
+const name = 'TodoList'
+
+async function main() {
+  const Contract = await hre.ethers.getContractFactory(name)
+  const contract = await Contract.deploy()
+  await contract.waitForDeployment()
+  console.log(`Contract deployed to: ${await contract.getAddress()}`)
+}
+
+main().catch(error => {
+  console.error(error)
+  process.exitCode = 1
+})
